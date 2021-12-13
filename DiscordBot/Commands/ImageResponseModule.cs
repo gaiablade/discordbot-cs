ed93bfdaf6bc13b8af4f5d24a1196daefdf86899
gaiablade <cgandeg@gmail.com>
2021-12-12 21:39:39 -0500

    Added Mario command

--- DiscordBot/Commands/ImageResponseModule.cs
+++ DiscordBot/Commands/ImageResponseModule.cs
@@ -11,15 +11,8 @@ namespace DiscordBot.Commands
 {
     public class ImageResponseModule : ModuleBase<SocketCommandContext>
     {
-        private static string[] luigiImageFilenames = GetLuigiImagesFilenames();
-        private static string[] GetLuigiImagesFilenames()
-        {
-            if (!Directory.Exists("Files/Images/Luigi"))
-            {
-                throw new FileNotFoundException("Directory does not exist");
-            }
-            return Directory.GetFiles("Files/Images/Luigi");
-        }
+        private static string[] luigiImageFilenames = Directory.GetFiles("Files/Images/Luigi");
+        private static string[] marioImageFilenames = Directory.GetFiles("Files/Images/Mario");
 
         [Command("luigi")]
         public async Task LuigiImageAsync()
@@ -29,6 +22,17 @@ namespace DiscordBot.Commands
 
             await Context.Channel.SendFileAsync(filePath: imageFilename, 
                 text: "Here's a Luigi :)", 
+                messageReference: new MessageReference(Context.Message.Id));
+        }
+
+        [Command("mario")]
+        public async Task MarioImageAsync()
+        {
+            var randomIndex = DiscordBot.Random.Next(marioImageFilenames.Length);
+            var imageFilename = marioImageFilenames[randomIndex];
+
+            await Context.Channel.SendFileAsync(filePath: imageFilename,
+                text: "Here's a Mario :D",
                 messageReference: new MessageReference(Context.Message.Id));
         }
     }
