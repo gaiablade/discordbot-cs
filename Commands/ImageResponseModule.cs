0bde9eb2b9744848017266ccee3f448ce619a68b
gaiablade <cgandeg@gmail.com>
2021-12-12 21:10:15 -0500

    Initial Commit

---
+++ Commands/ImageResponseModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Commands
+{
+    public class ImageResponseModule : ModuleBase<SocketCommandContext>
+    {
+        private static string[] luigiImageFilenames = GetLuigiImagesFilenames();
+        private static string[] GetLuigiImagesFilenames()
+        {
+            if (!Directory.Exists("Files/Images/Luigi"))
+            {
+                throw new FileNotFoundException("Directory does not exist");
+            }
+            return Directory.GetFiles("Files/Images/Luigi");
+        }
+
+        [Command("luigi")]
+        public async Task LuigiImageAsync()
+        {
+            var randomIndex = DiscordBot.Random.Next(luigiImageFilenames.Length);
+            var imageFilename = luigiImageFilenames[randomIndex];
+
+            await Context.Channel.SendFileAsync(filePath: imageFilename, 
+                text: "Here's a Luigi :)", 
+                messageReference: new MessageReference(Context.Message.Id));
+        }
+    }
+}
