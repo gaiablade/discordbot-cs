e12c262e4b2deeee0c01a41bfb43f76c9906b332
gaiablade <cgandeg@gmail.com>
2021-12-13 20:11:20 -0500

    Changed random number generator to use a cryprographic algorithm, added special case for dice rolling command

--- DiscordBotCS/Program.cs
+++ DiscordBotCS/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 using Discord;
 using Discord.API;
@@ -11,15 +12,15 @@ using Discord.Commands;
 using Discord.Net;
 using Discord.WebSocket;
 
-using DiscordBot.Debug;
+using DiscordBotCS.Debug;
 
-namespace DiscordBot
+namespace DiscordBotCS
 {
     public class DiscordBot
     {
         public static LogManager LogManager { get; private set; } = new LogManager(@"logs\bot\Discord Bot - [DT].txt");
-        public static Random Random { get; private set; } = new Random();
-        private DiscordSocketClient? client;
+        public static RandomNumberGenerator RandomNumberGenerator { get; private set; } = RandomNumberGenerator.Create();
+        public static DiscordSocketClient? Client { get; private set; }
         private CommandService? commandService;
 
         // Program Entry-Point
@@ -31,21 +32,21 @@ namespace DiscordBot
             // Import credentials (bot token) from file
             Credentials credentials = Credentials.GetCredentials("credentials.json");
 
-            // Create bot client to receive and respond to messages
-            client = new DiscordSocketClient();
-            client.Log += LogManager.LogDiscordMessage; // Assigns function that handles any log messages the bot receives
-            client.MessageReceived += HandleMessageReceivedAsync; // Assigns function that handles messages the bot receives
+            // Create bot Client to receive and respond to messages
+            Client = new DiscordSocketClient();
+            Client.Log += LogManager.LogDiscordMessage; // Assigns function that handles any log messages the bot receives
+            Client.MessageReceived += HandleMessageReceivedAsync; // Assigns function that handles messages the bot receives
 
             // Login and start bot
-            await client.LoginAsync(TokenType.Bot, credentials.token);
-            await client.StartAsync();
+            await Client.LoginAsync(TokenType.Bot, credentials.token);
+            await Client.StartAsync();
 
             // Create command service, find and load all commands
             commandService = new CommandService();
             await commandService.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
 
             // Set status message of bot
-            await client.SetActivityAsync(new Game("Being rewritten in C#...", ActivityType.Playing, ActivityProperties.None));
+            await Client.SetActivityAsync(new Game("Being rewritten in C#...", ActivityType.Playing, ActivityProperties.None));
 
             // Delays indefinitely to keep bot running
             await Task.Delay(-1);
@@ -62,16 +63,32 @@ namespace DiscordBot
             var message = messageParam as SocketUserMessage;
             if (message == null) return;
 
-            // Check if message is a command and isn't coming from another bot
             int argPos = 0;
-            if (!(message.HasCharPrefix('!', ref argPos)
-                || message.HasMentionPrefix(client?.CurrentUser, ref argPos))
+
+
+            // Check if message is a command and isn't coming from another bot
+            if (!(message.HasCharPrefix('!', argPos: ref argPos)
+                || message.HasMentionPrefix(Client?.CurrentUser, ref argPos))
                 || message.Author.IsBot)
                 return;
 
+            // Handle dice roll command separately, NEEDS A BETTER SOLUTION
+            // NOTE: Discord.NET's CommandService does not support commands that match regex patterns, they normally require spaces before parameters
+            //  Because of this, the roll command must be handled manually.
+            if (message.HasStringPrefix("!roll", ref argPos))
+            {
+                await DiceRollModule.DiceRoll(message);
+                return;
+            }
+
             // Search for corresponding command and execute it
-            var context = new SocketCommandContext(client, message);
+            var context = new SocketCommandContext(Client, message);
             await commandService.ExecuteAsync(context: context, argPos: argPos, services: null);
+        }
+
+        public static int GetRandomNumber(int toExclusive)
+        {
+            return RandomNumberGenerator.GetInt32(toExclusive);
         }
     }
 }
