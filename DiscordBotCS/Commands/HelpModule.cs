17cac05ae723072132889944d5530f43611aca23
gaiablade <cgandeg@gmail.com>
2021-12-13 20:13:11 -0500

    Added help command that lists all available commands

---
+++ DiscordBotCS/Commands/HelpModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot_CS.Commands
+{
+    public class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task HelpAsync()
+        {
+            string helpInformation =
+                "List Of Commands\n" +
+                "1) !mario\n" +
+                "2) !luigi\n" +
+                "3) !roll[numDice]d[numSides][+/-][number]\n" +
+                "4) !help";
+
+            await ReplyAsync(helpInformation);
+        }
+    }
+}
