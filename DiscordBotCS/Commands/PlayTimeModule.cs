78f331b7233ed34adb4410bff716df6bcd8900aa
gaiablade <cgandeg@gmail.com>
2022-01-06 23:17:11 -0500

    Added popular games command, now differentiates based on server

--- DiscordBotCS/Commands/PlayTimeModule.cs
+++ DiscordBotCS/Commands/PlayTimeModule.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Discord;
+﻿using Discord;
 using Discord.Commands;
 
-using DiscordBotCS.Commands;
 using DiscordBotCS.DatabaseInteraction;
 
 namespace DiscordBotCS.Commands
@@ -29,33 +22,75 @@ namespace DiscordBotCS.Commands
             switch (action)
             {
                 case "wrapped":
-                    // Get top 5 played games:
-                    List<(string name, int minutesPlayed)> games = await PlayTimeRecorder.GetPlayTimeWrappedAsync(Context.User.Id, limit: 5);
-                    if (games.Count == 0)
                     {
-                        await ReplyAsync("My database doesn't contain any information of your play history!");
+                        var limit = 5;
+                        if (parameters.Length > 1)
+                        {
+                            var parsed = int.TryParse(parameters[1], out limit);
+                            if (parsed) limit = Math.Min(limit, 20);
+                            else limit = 5;
+                        }
+
+                        // Get top X played games:
+                        List<(string name, int minutesPlayed)> games = await PlayTimeRecorder.GetPlayTimeWrappedAsync(Context.User.Id, Context.Guild.Id, limit);
+                        if (games.Count == 0)
+                        {
+                            await ReplyAsync("My database doesn't contain any information of your play history!");
+                            break;
+                        }
+
+                        // Create embed
+                        var embed = new EmbedBuilder()
+                        {
+                            Title = $"{Context.User.Username}'s Most-Played Games"
+                        };
+                        for (int i = 0; i < games.Count; i++)
+                        {
+                            double hoursPlayed = Math.Round((double)(games[i].minutesPlayed) / 60.0, 2);
+                            embed = embed.AddField(new EmbedFieldBuilder()
+                            {
+                                Name = $"#{i + 1}) {games[i].name}",
+                                Value = $"Hours: {hoursPlayed}"
+                            });
+                        }
+                        await ReplyAsync(embed: embed.Build());
                         break;
                     }
-
-                    // Create embed
-                    var embed = new EmbedBuilder()
+                case "popular":
                     {
-                        Title = $"{Context.User.Username}'s Most-Played Games"
-                    };
-                    for (int i = 0; i < games.Count; i++)
-                    {
-                        double hoursPlayed = Math.Round((double)(games[i].minutesPlayed) / 60.0, 2);
-                        embed = embed.AddField(new EmbedFieldBuilder()
+                        var limit = 5;
+                        if (parameters.Length > 1)
                         {
-                             Name = $"#{i + 1}) {games[i].name}",
-                             Value = $"Hours: {hoursPlayed}",
-                             IsInline = true,
-                        });
+                            var parsed = int.TryParse(parameters[1], out limit);
+                            if (parsed) limit = Math.Min(limit, 20);
+                            else limit = 5;
+                        }
+
+                        // Get top X popular games
+                        var popularGames = await PlayTimeRecorder.GetPopularGames(Context.Guild.Id, limit);
+                        if (popularGames.Count == 0)
+                        {
+                            await ReplyAsync("No stats recorded yet!");
+                            break;
+                        }
+
+                        var popularGamesEmbed = new EmbedBuilder()
+                        {
+                            Title = "Most Popular Games this Year!"
+                        };
+                        for (int i = 0; i < popularGames.Count; i++)
+                        {
+                            double totalHours = Math.Round((double)(popularGames[i].minutes) / 60.0, 2);
+                            popularGamesEmbed.AddField(new EmbedFieldBuilder()
+                            {
+                                Name = $"#{i + 1}) {popularGames[i].game}",
+                                Value = $"Total Hours: {totalHours}, Players: {popularGames[i].numPlayers}"
+                            });
+                        }
+
+                        await ReplyAsync(embed: popularGamesEmbed.Build());
+                        break;
                     }
-              
-                    
-                    await ReplyAsync(embed: embed.Build());
-                    break;
                 default:
                     await ReplyAsync("Unrecognized playtime command!");
                     break;
--- DiscordBotCS/DatabaseInteraction/PlayTimeRecorder.cs
+++ DiscordBotCS/DatabaseInteraction/PlayTimeRecorder.cs
@@ -1,6 +1,4 @@
-﻿using MySql;
-using MySql.Data;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 
 namespace DiscordBotCS.DatabaseInteraction
 {
@@ -8,71 +6,81 @@ namespace DiscordBotCS.DatabaseInteraction
     {
         private static void LogQuery(MySqlCommand command)
         {
-            Console.WriteLine(command.CommandText);
+            string commandText = command.CommandText;
+            foreach (MySqlParameter parameter in command.Parameters)
+            {
+                commandText = commandText.Replace(parameter.ParameterName, parameter.Value.ToString());
+            }
+            Console.WriteLine(commandText);
         }
 
         // TODO: USE ENTITY FRAMEWORK
 
-        private static void DeleteActiveGame(ulong id, MySqlConnection connection)
+        private static void DeleteActiveGame(ulong id, ulong serverID, MySqlConnection connection)
         {
-            var deleteCommand = "delete from active_games where user_id = @id";
+            var deleteCommand = "delete from active_games where user_id = @id and server_id = @server_id";
             var command = new MySqlCommand(deleteCommand, connection);
             command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@server_id", serverID);
             LogQuery(command);
             command.ExecuteNonQuery();
         }
 
-        private static void CreateActiveGame(ulong id, string gameTitle, MySqlConnection connection)
+        private static void CreateActiveGame(ulong id, ulong serverID, string gameTitle, MySqlConnection connection)
         {
             var now = DateTime.Now;
-            string insertCommand = "insert into active_games (user_id, game_title, time_started) values (@id, @title, @now)";
+            string insertCommand = "insert into active_games (user_id, server_id, game_title, time_started) values (@id, @server_id, @title, @now)";
             var command = new MySqlCommand(insertCommand, connection);
             command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@server_id", serverID);
             command.Parameters.AddWithValue("@title", gameTitle);
             command.Parameters.AddWithValue("@now", now);
             LogQuery(command);
             command.ExecuteNonQuery();
         }
 
-        private static MySqlDataReader GetActiveGame(ulong id, string gameTitle, MySqlConnection connection)
+        private static MySqlDataReader GetActiveGame(ulong id, ulong serverID, string gameTitle, MySqlConnection connection)
         {
-            var query = "select user_id, game_title, time_started from active_games where user_id = @id and game_title = @title order by time_started desc";
+            var query = "select user_id, game_title, time_started from active_games where user_id = @id and server_id = @server_id and game_title = @title order by time_started desc";
             var dbCommand = new MySqlCommand(query, connection);
             dbCommand.Parameters.AddWithValue("@id", id);
+            dbCommand.Parameters.AddWithValue("@server_id", serverID);
             dbCommand.Parameters.AddWithValue("@title", gameTitle);
             LogQuery(dbCommand);
             return dbCommand.ExecuteReader();
         }
 
-        private static bool TotalGameTimeExists(ulong id, string gameTitle, MySqlConnection connection)
+        private static bool TotalGameTimeExists(ulong id, ulong serverID, string gameTitle, MySqlConnection connection)
         {
-            var query = "select count(*) from in_game_time where user_id = @id and game_title = @title";
+            var query = "select count(*) from in_game_time where user_id = @id and server_id = @server_id and game_title = @title";
             var dbCommand = new MySqlCommand(query, connection);
             dbCommand.Parameters.AddWithValue("@id", id);
+            dbCommand.Parameters.AddWithValue("@server_id", serverID);
             dbCommand.Parameters.AddWithValue("@title", gameTitle);
-            //using var ingameTimeReader = dbCommand.ExecuteReader();
             LogQuery(dbCommand);
             var count = (Int64)dbCommand.ExecuteScalar();
             return count > 0;
         }
 
-        private static void CreateGameTimeRecord(ulong id, string gameTitle, int minutesPlayed, MySqlConnection connection)
+        private static void CreateGameTimeRecord(ulong id, ulong serverID, string gameTitle, int minutesPlayed, MySqlConnection connection)
         {
-            var query = "insert into in_game_time (game_title, user_id, minutes) values (@title, @id, @minPlayed)";
+            var query = "insert into in_game_time (game_title, user_id, server_id, minutes) values (@title, @id, @server_id, @minPlayed)";
             var dbCommand = new MySqlCommand(query, connection);
             dbCommand.Parameters.AddWithValue("@minPlayed", minutesPlayed);
             dbCommand.Parameters.AddWithValue("@id", id);
+            dbCommand.Parameters.AddWithValue("@server_id", serverID);
             dbCommand.Parameters.AddWithValue("@title", gameTitle);
             LogQuery(dbCommand);
             dbCommand.ExecuteNonQuery();
         }
 
-        private static void UpdateGameTimeRecord(ulong id, string gameTitle, int minutesPlayed, MySqlConnection connection)
+        private static void UpdateGameTimeRecord(ulong id, ulong serverID, string gameTitle, int minutesPlayed, MySqlConnection connection)
         {
-            var query = "update in_game_time set minutes = minutes + @minPlayed where user_id = @id and game_title = @title";
+            var query = "update in_game_time set minutes = minutes + @minPlayed where user_id = @id and server_id = @server_id and game_title = @title";
             var dbCommand = new MySqlCommand(query, connection);
             dbCommand.Parameters.AddWithValue("@minPlayed", minutesPlayed);
             dbCommand.Parameters.AddWithValue("@id", id);
+            dbCommand.Parameters.AddWithValue("@server_id", serverID);
             dbCommand.Parameters.AddWithValue("@title", gameTitle);
             LogQuery(dbCommand);
             dbCommand.ExecuteNonQuery();
@@ -110,6 +118,36 @@ namespace DiscordBotCS.DatabaseInteraction
             command.ExecuteNonQuery();
         }
 
+        private static bool ServerExists(ulong id, MySqlConnection connection)
+        {
+            var query = "select count(*) from server_header where server_id = @id";
+            var command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            LogQuery(command);
+            var result = (Int64)command.ExecuteScalar();
+            return result > 0;
+        }
+
+        private static void UpdateServer(ulong id, string serverName, MySqlConnection connection)
+        {
+            var query = "update server_header set server_name = @name where server_id = @id";
+            var command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@name", serverName);
+            command.Parameters.AddWithValue("@id", id);
+            LogQuery(command);
+            command.ExecuteNonQuery();
+        }
+
+        private static void CreateServer(ulong id, string name, MySqlConnection connection)
+        {
+            var query = "insert into server_header (server_id, server_name) values (@id, @name)";
+            var command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@name", name);
+            LogQuery(command);
+            command.ExecuteNonQuery();
+        }
+
         private struct ActiveGame
         {
             public ulong Id;
@@ -117,11 +155,11 @@ namespace DiscordBotCS.DatabaseInteraction
             public DateTime TimeStarted;
         }
 
-        public static async Task UpdatePlayTime(ulong userID, string username, string? before, string? after)
+        public static async Task UpdatePlayTime(ulong userID, string username, ulong serverID, string serverName, string? before, string? after)
         {
             await Task.Run(() =>
             {
-                var ConnectionString = DiscordBot.Credentials.connectionString;
+                var ConnectionString = DiscordBot.Credentials.ConnectionString;
                 using var connection = new MySqlConnection(ConnectionString);
                 connection.Open();
 
@@ -135,19 +173,29 @@ namespace DiscordBotCS.DatabaseInteraction
                     CreateUser(userID, username, connection);
                 }
 
+                // Add / update server_header
+                if (ServerExists(serverID, connection))
+                {
+                    UpdateServer(serverID, serverName, connection);
+                }
+                else
+                {
+                    CreateServer(serverID, serverName, connection);
+                }
+
                 // User started playing a game
                 if (before == null && after != null)
                 {
                     // An active game should not exist, but if it does, delete it
-                    DeleteActiveGame(userID, connection);
-                    CreateActiveGame(userID, after.Substring(0, 30 - (30 - after.Length)), connection);
+                    DeleteActiveGame(userID, serverID, connection);
+                    CreateActiveGame(userID, serverID, after.Substring(0, 30 - (30 - after.Length)), connection);
                 }
                 // User stopped playing a game
                 else if (before != null && after == null)
                 {
                     string gameTitle = before.Substring(0, 30 - (30 - before.Length));
                     List<ActiveGame> activeGames = new List<ActiveGame>();
-                    using (var activeGamesReader = GetActiveGame(userID, gameTitle, connection))
+                    using (var activeGamesReader = GetActiveGame(userID, serverID, gameTitle, connection))
                     {
                         if (!activeGamesReader.HasRows)
                         {
@@ -169,14 +217,14 @@ namespace DiscordBotCS.DatabaseInteraction
                         var minutesPlayed = (int)((timeEnded - activeGame.TimeStarted).TotalMinutes);
 
                         // Check if there is a record for this user and game, if not, create it
-                        var recordExists = TotalGameTimeExists(userID, gameTitle, connection);
+                        var recordExists = TotalGameTimeExists(userID, serverID, gameTitle, connection);
 
                         if (!recordExists)
-                            CreateGameTimeRecord(userID, gameTitle, minutesPlayed, connection);
+                            CreateGameTimeRecord(userID, serverID, gameTitle, minutesPlayed, connection);
                         else
-                            UpdateGameTimeRecord(userID, gameTitle, minutesPlayed, connection);
+                            UpdateGameTimeRecord(userID, serverID, gameTitle, minutesPlayed, connection);
 
-                        DeleteActiveGame(userID, connection);
+                        DeleteActiveGame(userID, serverID, connection);
                     }
 
                 }
@@ -195,7 +243,7 @@ namespace DiscordBotCS.DatabaseInteraction
                     // User switched games
                     {   // Update game that was being played before
                         List<ActiveGame> activeGames = new List<ActiveGame>();
-                        using (var activeGameReader = GetActiveGame(userID, beforeGameTitle, connection))
+                        using (var activeGameReader = GetActiveGame(userID, serverID, beforeGameTitle, connection))
                         {
                             while (activeGameReader.Read())
                             {
@@ -209,18 +257,18 @@ namespace DiscordBotCS.DatabaseInteraction
                             var minutesPlayed = (int)((timeEnded - activeGame.TimeStarted).TotalMinutes);
 
                             // Check if there is a record for this user and game, if not, create it
-                            var recordExists = TotalGameTimeExists(userID, beforeGameTitle, connection);
+                            var recordExists = TotalGameTimeExists(userID, serverID, beforeGameTitle, connection);
 
                             if (!recordExists)
-                                CreateGameTimeRecord(userID, beforeGameTitle, minutesPlayed, connection);
+                                CreateGameTimeRecord(userID, serverID, beforeGameTitle, minutesPlayed, connection);
                             else
-                                UpdateGameTimeRecord(userID, beforeGameTitle, minutesPlayed, connection);
+                                UpdateGameTimeRecord(userID, serverID, beforeGameTitle, minutesPlayed, connection);
                         }                        
                     }
 
                     {   // Create new active game
-                        DeleteActiveGame(userID, connection);
-                        CreateActiveGame(userID, afterGameTitle, connection);
+                        DeleteActiveGame(userID, serverID, connection);
+                        CreateActiveGame(userID, serverID, afterGameTitle, connection);
                     }
                 }
                 // The update is triggered by something other than game status update, ignore this
@@ -234,15 +282,16 @@ namespace DiscordBotCS.DatabaseInteraction
             });
         }
 
-        public static async Task<List<(string name, int minutesPlayed)>> GetPlayTimeWrappedAsync(ulong id, int limit = 5)
+        public static async Task<List<(string name, int minutesPlayed)>> GetPlayTimeWrappedAsync(ulong id, ulong serverID, int limit = 5)
         {
-            var ConnectionString = DiscordBot.Credentials.connectionString;
+            var ConnectionString = DiscordBot.Credentials.ConnectionString;
             using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                var query = "select game_title, minutes from in_game_time where user_id = @id order by minutes desc limit @limit";
+                var query = "select game_title, minutes from in_game_time where user_id = @id and server_id = @server_id order by minutes desc limit @limit";
                 var command = new MySqlCommand(query, connection);
                 command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@server_id", serverID);
                 command.Parameters.AddWithValue("@limit", limit);
                 LogQuery(command);
 
@@ -259,6 +308,45 @@ namespace DiscordBotCS.DatabaseInteraction
                         result.Add((reader.GetString(0), reader.GetInt32(1)));
                     }
                     return result;
+                }
+            }
+        }
+
+        public static async Task<List<(string game, int minutes, int numPlayers)>> GetPopularGames(ulong serverID, int limit = 5)
+        {
+            var ConnectionString = DiscordBot.Credentials.ConnectionString;
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+                string query = "select Game, `Minutes Played`, `Number of Players` from v_popular_games where `Server ID` = @server_id limit @limit";
+                var command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@server_id", serverID);
+                command.Parameters.AddWithValue("@limit", limit);
+                LogQuery(command);
+
+                try
+                {
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("No Rows");
+                            return new List<(string game, int minutes, int numPlayers)> { };
+                        }
+
+                        var result = new List<(string name, int minutes, int numPlayers)>();
+                        while (reader.Read())
+                        {
+                            result.Add((reader.GetString(0), reader.GetInt32(1), reader.GetInt32(2)));
+                        }
+                        Console.WriteLine($"result contained {result.Count} games");
+                        return result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return new List<(string game, int minutes, int numPlayers)> { };
                 }
             }
         }
